c6700ee4990d13610d63281d8c3a7474dfdf3d10
david <davidjohansson35@gmail.com>
2019-12-23 06:59:23 +0100

    simple_sender sends a value

--- hjdj_cpp/targets/simple_sender.cpp
+++ hjdj_cpp/targets/simple_sender.cpp
@@ -12,4 +12,9 @@ int main(int argc, char const *argv[]){
   unsigned int port = atoi(argv[1]);
   char const *ip_address = argv[2];
   hjdj::Sender<int> sender(port, 1024, ip_address);
+
+  int send_value = 7;
+
+  sender.Send(send_value);
+  LOG(INFO) << "Sent value: " << send_value;
 }
